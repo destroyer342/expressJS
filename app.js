031a0aec4e48649fa5b85b806e266d7e560d8a49
destroyer342 <ranceofficial@gmail.com>
2022-02-27 10:46:22 +0800

    make it better

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express")
+const app = express()
+
+let port = process.env.PORT || 8010;
+
+const function1 = () => {
+    console.log(`App is running at the port ${port}`) ;
+ }
+
+app.listen(port,function1);
+app.get('/', (req, res) =>{
+res.send('Hello rance')
+
+})
